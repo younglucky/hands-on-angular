ccb43e38265d56474676f8033f9d5fec0d64c522
Thomas Fankhauser <tommy@southdesign.de>
2013-03-25 16:32:20 +0100

    extended specs to have a find and all method

--- services/pictures.js
+++ services/pictures.js
@@ -1,10 +1,10 @@
 gallery.service("pictures", function(){
   
   this.all = [
-    { name: "First Picture",  url: "1.png" },
-    { name: "Second Picture", url: "2.png" },
-    { name: "Third Picture",  url: "3.png" },
-    { name: "Fourth Picture", url: "4.png" }
+    { id: 1, name: "First Picture",  url: "1.png" },
+    { id: 2, name: "Second Picture", url: "2.png" },
+    { id: 3, name: "Third Picture",  url: "3.png" },
+    { id: 4, name: "Fourth Picture", url: "4.png" }
   ];
   
 });
--- specs/picturesServiceSpec.js
+++ specs/picturesServiceSpec.js
@@ -14,4 +14,21 @@ describe("Pictures Service", function(){
     expect(pictures.all.length).toBe(4);
   });
   
+  it("returns a picture by id", function(){
+    expect(pictures.find(1)).toBe({ id: 1, name: "First Picture",  url: "1.png" });
+    expect(pictures.find(3)).toBe({ id: 3, name: "Third Picture",  url: "3.png" });
+  });
+  
+  it("allows to add a picture", function(){
+    expect(pictures.all.length).toBe(4);
+    pictures.add({ id: 5, name: "Fifth Picture", url: "5.png" });
+    expect(pictures.all.length).toBe(5);
+  });
+  
+  it("prohibits to add pictures that are already in the storage", function(){
+    expect(pictures.all.length).toBe(4);
+    pictures.add({ id: 4, name: "Fourth Picture", url: "4.png" });
+    expect(pictures.all.length).toBe(4);
+  });
+  
 });
