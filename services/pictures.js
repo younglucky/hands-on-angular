eb471ffa5d72034659abd429a4234adb25fdc7be
Thomas Fankhauser <tommy@southdesign.de>
2013-03-25 16:27:50 +0100

    initial picture service with default pictures

---
+++ services/pictures.js
@@ -0,0 +1,10 @@
+gallery.service("pictures", function(){
+  
+  this.all = [
+    { name: "First Picture",  url: "1.png" },
+    { name: "Second Picture", url: "2.png" },
+    { name: "Third Picture",  url: "3.png" },
+    { name: "Fourth Picture", url: "4.png" }
+  ];
+  
+});
